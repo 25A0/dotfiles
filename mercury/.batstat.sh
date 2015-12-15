63881345800d2bba36f8ea32165f698b4a670af0
Moritz Neikes <mneikes@gmx.de>
2015-12-15 21:26:07 +0100

    Add custom config files for mercury

---
+++ mercury/.batstat.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# This script is used to write "LOW BAT" to a file once
+# the battery charge drops below 20%.
+# I use a cronjob to run this script every minute,
+# and a custom zsh theme to display the content of
+# the file ~/.batstat.txt in my prompt.
+# This way I'll notice a low battery even without having
+# to check the OS X status bar.
+# 
+# This is the custom prompt:
+# 
+# PROMPT='%? %{$fg_bold[red]%}%m%{$reset_color%}:%{$fg[cyan]%}%c%{$reset_color%}:%# '
+# RPROMPT='%{$fg_bold[red]%}$(command cat ~/.batstat.txt)%{$reset_color%}%{$fg_bold[green]%} $(git_prompt_info)%{$reset_color%} '
+
+IOREG=/usr/sbin/ioreg
+Max=`${IOREG} -rc AppleSmartBattery | grep -e MaxCapacity | tr -s " " : | cut -d":" -f4`
+Current=`${IOREG} -rc AppleSmartBattery | grep -e CurrentCapacity | tr -s " " : | cut -d":" -f4`
+# echo Max: ${Max}
+# echo Current: ${Current}
+Charge=$((100*$Current/$Max))
+# echo ${Charge}
+Text=
+if [[ $Charge -le 20 ]]; then
+	Text="LOW BAT"
+# else
+# 	Text="FULL BAT"
+fi
+
+printf "${Text}" > /Users/moritz/.batstat.txt
