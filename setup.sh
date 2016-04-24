c84dcf077c7683147e1714d7c0a17e2d0833d83e
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2016-04-24 19:43:09 +0200

    Add script to set up dotfiles on new host

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Creates a link from $1 to $2, and 
+f_link()
+{
+    ln -is $1 $2
+}
+
+# Create link to .zshrc
+f_link `pwd`/.zshrc ${HOME}/.zshrc
+
+HOST=`hostname`
+if [[ -d ./${HOST} ]]; then
+    # Link to individual files in ${HOST}
+    cd ./${HOST} && for file in `find . -type f`; do \
+        echo Linking ${file}; \
+        src=`pwd`/${file}
+        dest=${HOME}/${file}
+        f_link `realpath -s ${src}` `realpath -s ${dest}`; \
+    done
+fi
